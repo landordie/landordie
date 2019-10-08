33eeb87d346bce9dcdda8b8b2c5db7721fe53e9f
landordie <56274397+landordie@users.noreply.github.com>
2019-10-08 18:37:24 +0100

    Add files via upload

--- constants.py
+++ constants.py
@@ -2,9 +2,22 @@
 Constants here
 """
 
+# ------------------------COLORS
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+# ------------------------BUTTON SIZE
+BUTTON_WIDTH = 220
+BUTTON_HEIGHT = 60
+
+# ------------------------SCENE SIZES
+T_SCREEN_WIDTH = 800
+T_SCREEN_HEIGHT = 600
+
+G_SCREEN_WIDTH = 1280
+G_SCREEN_HEIGHT = 720
+
