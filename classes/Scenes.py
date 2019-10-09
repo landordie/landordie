11003428a03c2c520604a54c59e2e7cc66afb0d2
landordie <56274397+landordie@users.noreply.github.com>
2019-10-09 14:39:44 +0100

    Add files via upload

--- classes/Scenes.py
+++ classes/Scenes.py
@@ -1,9 +1,10 @@
 import pygame
-from PyMunk.classes.button import *
-from PyMunk.load_images import load_images
-from PyMunk.load_images import update as title_update
+from classes.button import *
+from load_images import load_images
+from load_images import update as title_update
+from constants import *
 
-
+pygame.font.init()
 # (!) REMEMBER (!)
 # When changing scenes, resolution is hardcoded
 #
@@ -21,7 +22,7 @@ def __text_objects(text, font):
 #       text -> string you want displayed
 #       size -> the size of the string (different lengths need different size)
 #       font -> name of the font
-def displayText(screen, text, font, size):
+def display_text(screen, text, font, size):
     screen = screen.get_surface()
     width, height = screen.get_size()
     font = pygame.font.Font(font, size)
@@ -34,6 +35,12 @@ def displayText(screen, text, font, size):
 class SceneBase:
     def __init__(self):
         self.next = self
+        # Fonts:
+        self.font_arial_black = pygame.font.SysFont('Arial Black', 18)
+        self.font_verdana = pygame.font.SysFont('Verdana', 35)
+        self.font_arial_black_large = pygame.font.SysFont('Arial Black', 50)
+        self.font_verily_mono = pygame.font.SysFont('Verily Serif Mono', 27)
+        self.font_freesans_bold = pygame.font.SysFont("Freesans Bold", 35)
 
     # Using this method mainly for testing
     # The game logic will be implemented in the Update() method
@@ -52,6 +59,15 @@ class SceneBase:
     def Terminate(self):
         self.SwitchToScene(None)
 
+    @staticmethod
+    def scene_wh(screen):
+        """
+        Scene width and height
+        """
+        width = screen.get_surface().get_width()
+        height = screen.get_surface().get_height()
+        return width, height
+
 
 class TitleScene(SceneBase):
     def __init__(self):
@@ -63,27 +79,20 @@ class TitleScene(SceneBase):
                 self.Terminate()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                 self.SwitchToScene(GameScene())
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.SwitchToScene(SplashScene())
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 pg.mixer.Sound("blipshort1.wav").play()
 
     def Update(self):
         pass
 
-    @staticmethod
-    def scene_wh(screen):
-        """
-        Scene width and height
-        """
-        width = screen.get_surface().get_width()
-        height = screen.get_surface().get_height()
-        return width, height
-
     # (!) When adding buttons to the start screen
     # (!) Add them here, in this method
     def Render(self, screen):
 
         # Width and height unpacking
-        w, h = TitleScene.scene_wh(screen)
+        w, h = self.scene_wh(screen)
 
         # Start and Quit buttons
         menu_button = Button((w / 2 - (BUTTON_WIDTH/2), h / 2, BUTTON_WIDTH, BUTTON_HEIGHT), YELLOW, 'Start')
@@ -100,7 +109,49 @@ class TitleScene(SceneBase):
 
         # Update title
         title_update(title_surfaces, screen)
-        # displayText(screen, "Land or Die", 'freesansbold.ttf', 75)
+
+
+class SplashScene(SceneBase):
+    def __init__(self):
+        SceneBase.__init__(self)
+
+    def ProcessInput(self, events, pressed_keys):
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.Terminate()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.SwitchToScene(GameScene())
+
+    def Update(self):
+        pass
+
+    def Render(self, screen):
+        screen.set_mode((1280, 720))
+        w, h = self.scene_wh(screen)
+        splash_w, splash_h = 700, 630
+        splash_x, splash_y = (w/2) - (splash_w/2), (h/2) - (splash_h/2)
+
+        controls_background = pygame.Surface((splash_w, splash_h)).convert_alpha()
+        controls_background.fill(BLACK_HIGHLIGHT)
+        logo = pygame.image.load('frames/Landordie.png')
+        background = pygame.image.load('frames/backgr.png')
+
+        screen.get_surface().fill(WHITE)
+
+        screen.get_surface().blit(background, (0, 0))
+        screen.get_surface().blit(controls_background, (splash_x, splash_y, splash_w, splash_h))
+        screen.get_surface().blit(logo, (splash_x, splash_y))
+
+        self.draw_text(screen, "Game Controls", (splash_x + (splash_w/3), splash_y), self.font_arial_black_large, (140, 123, 192))
+        self.draw_text(screen, "Player 1: ", (splash_x + 100, splash_y + 100), self.font_verdana, (255, 255, 255))
+        self.draw_text(screen, "Player 2: ", (splash_x + 450, splash_y + 100), self.font_verdana, (255, 255, 255))
+
+        pygame.display.update()
+
+    @staticmethod
+    def draw_text(screen, message, position, font, color=(0, 0, 0)):
+        text = font.render(message, False, color)
+        screen.get_surface().blit(text, position)
 
 
 class GameScene(SceneBase):
@@ -136,7 +187,7 @@ class WinScene(SceneBase):
     def Render(self, screen):
         screen.set_mode((800, 600))
         screen.get_surface().fill(GREEN)
-        displayText(screen, "Congratulations!"
+        display_text(screen, "Congratulations!"
                             "You have won!", 'freesansbold.ttf', 50)
 
 
@@ -153,4 +204,4 @@ class LoseScene(SceneBase):
     def Render(self, screen):
         screen.set_mode((800, 600))
         screen.get_surface().fill(RED)
-        displayText(screen, "Too bad! You have lost!", 'freesansbold.ttf', 45)
+        display_text(screen, "Too bad! You have lost!", 'freesansbold.ttf', 45)
