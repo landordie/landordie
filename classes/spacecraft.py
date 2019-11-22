cc8461de9b47db6353d4ab551efd4aefdcd47609
landordie <56274397+landordie@users.noreply.github.com>
2019-11-22 01:09:56 +0000

    Update spacecraft.py

--- classes/spacecraft.py
+++ classes/spacecraft.py
@@ -1,3 +1,6 @@
+"""
+    Spacecraft Module (Sprite)
+"""
 from math import sin, cos, degrees
 import pygame as pg
 import pymunk as pm
@@ -11,41 +14,47 @@ def flipy(p):
 
 
 class Spacecraft(pg.sprite.Sprite):
-
+    """Spacecraft Instance Class"""
+    
     def __init__(self, pos):
         super().__init__()
-        self.crashed = False
+        
         # Drawing the shape
         self.image = pg.Surface((46, 52), pg.SRCALPHA)
         pg.draw.polygon(self.image, (0, 50, 200),
                         [(0, 0), (48, 0), (48, 54), (24, 54)])
         self.orig_image = self.image
         self.rect = self.image.get_rect(topleft=pos)
+        
         # The vertices of the shape
         vs = [(-23, 26), (23, 26), (23, -26), (0, -26)]
         mass = 0.4
         moment = pm.moment_for_poly(mass, vs)
+        
         # Attaching the shape to a body
         self.body = pm.Body(mass, moment)
         self.shape = pm.Poly(self.body, vs)
         self.shape.collision_type = 3
         self.body.position = pos
+        
+        # Bool to show the state of the spacecraft
+        self.crashed = False
 
     # Like the update method in the Scenes(), this update is called every frame
-    # and checks if rotation has been applied, if so it rotates the image(body/shape)
     def update(self):
+        """Check if rotation has been applied, if so it rotates the image(body/shape)"""
         pos = flipy(self.body.position)
         self.rect.center = pos
         self.image = pg.transform.rotate(
             self.orig_image, degrees(self.body.angle))
         self.rect = self.image.get_rect(center=self.rect.center)
 
-    # Used to apply force to rotate the ship to the left
     def rotate_left(self):
+        """Apply force to rotate the ship to the left"""
         self.body.angular_velocity = 0.5
 
-    # Used to apply force to rotate the ship to the right
     def rotate_right(self):
+        """Apply force to rotate the ship to the right"""
         self.body.angular_velocity = -0.5
 
     def move_up(self):
