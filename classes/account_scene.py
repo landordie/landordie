d3b6d1a46c77734cdb1ecc9fa0a49a853a800f96
denideni21 <denideni21@abv.bg>
2020-03-22 13:50:31 +0000

    yes

--- classes/account_scene.py
+++ classes/account_scene.py
@@ -67,6 +67,7 @@ class AccountScene(SceneBase):
         self.status = ''  # This variable indicates the status of the DB connection
         self.fields = [self.input_box1, self.input_box2, self.input_box3,
                        self.input_box4]  # A list used to group the input fields for cleaner code
+        self.cool_down = 0
 
     def ProcessInput(self, events, pressed_keys):
         for event in events:
@@ -82,11 +83,11 @@ class AccountScene(SceneBase):
                     menu = MenuScene.getInstance()
                     self.SwitchToScene(menu)
                 elif self.reg_sc_button.on_click(event):  # Register user with DB
-                    self.connect_db("register_sc", self.input_box1.text, self.input_box2.text)
+                    self.connect_db("register", self.input_box1.text, self.input_box2.text)
                 elif self.login_sc_button.on_click(event):  # Check if user in DB and sign them in if True
                     self.connect_db("login_sc", self.input_box1.text, self.input_box2.text)
                 elif self.reg_a_sc_button.on_click(event):  # Register user with DB
-                    self.connect_db("register_asc", self.input_box3.text, self.input_box4.text)
+                    self.connect_db("register", self.input_box3.text, self.input_box4.text)
                 elif self.login_a_sc_button.on_click(event):  # Check if user in DB and sign them in if True
                     self.connect_db("login_asc", self.input_box3.text, self.input_box4.text)
 
@@ -133,12 +134,13 @@ class AccountScene(SceneBase):
                        self.font_medium, WHITE)
 
         # If the status variable is changed (the value not equal to the initial one)
-        if self.status != '':
+        if self.status != '' and self.cool_down < 121:
             # Show the status on screen
             display.blit(self.status_cont,
                          (self.status_cont_x, self.status_cont_y, self.status_cont_w, self.status_cont_h))
             self.draw_text(screen, self.status, (self.screen_width / 2, self.screen_height / 10),
                            self.font_playernum, WHITE)
+            self.cool_down += 1
 
         # Draw credential fields on screen and reflect any changes to their content
         self.input_box1.draw(display)
@@ -157,7 +159,7 @@ class AccountScene(SceneBase):
         self.button_cont = pygame.Surface((self.button_cont_w, self.button_cont_h)).convert_alpha()
 
         # Container for the database response
-        self.status_cont_w, self.status_cont_h = self.screen_width / 1.2, self.screen_height / 15
+        self.status_cont_w, self.status_cont_h = self.screen_width, self.screen_height / 15
         self.status_cont_x, self.status_cont_y = (self.screen_width / 2 - self.status_cont_w / 2), (
                 self.screen_height / 15)
         self.status_cont = pygame.Surface((self.status_cont_w, self.status_cont_h)).convert_alpha()
@@ -187,6 +189,7 @@ class AccountScene(SceneBase):
 
             for field in [name, pw]:  # Check if the credential fields are empty. If so notify user and abort action
                 if field == '':
+                    self.cool_down = 0
                     self.status = 'You cannot submit an empty field. Please try again!'
                     return
 
@@ -204,32 +207,35 @@ class AccountScene(SceneBase):
                                 self.status = 'Spacecraft player signed in as [{0}]! Enjoy the game.'.format(name)
                                 SceneBase.credentials[0] = [name, pw]  # Update the state
                                 SceneBase.logged_in[0] = True
+                                self.cool_down = 0
                             else:
                                 self.status = 'Wrong credentials entered. Please check the input again.'
+                                self.cool_down = 0
                     finally:  # Always close the connection
                         connection.close()
                 else:
                     self.status = 'You are already signed in! Please log out first.'
+                    self.cool_down = 0
 
-            elif command == "register_sc":  # If the DB has to register a new user
-                if not SceneBase.logged_in[0]:  # and one is not currently logged into another account
-                    try:
-                        with connection.cursor() as cursor:  # Create a cursor object
-                            sql = "SELECT `Username` FROM `users`"  # Define the statement in SQL
-                            cursor.execute(sql)  # Execute it
-                            a = [row[0] for row in cursor.fetchall()]  # Fetch all username entries from the response
-                            if name in a:  # Check if a user with that name already exists
-                                self.status = 'A user with that username already exists. Please try again!'
-                            else:  # If not
-                                # Insert the current user and commit the changes to the server
-                                sql = "INSERT INTO `users` (`Username`, `Password`) VALUES (%s, %s)"
-                                cursor.execute(sql, (name, pw))
-                                connection.commit()
-                                self.status = 'Operation executed successfully!'
-                    finally:  # Always close the connection
-                        connection.close()
-                else:
-                    self.status = 'You are already signed in! Please log out to proceed.'
+            elif command == "register":  # If the DB has to register a new user
+                try:
+                    with connection.cursor() as cursor:  # Create a cursor object
+                        sql = "SELECT `Username` FROM `users`"  # Define the statement in SQL
+                        cursor.execute(sql)  # Execute it
+                        a = [row[0] for row in cursor.fetchall()]  # Fetch all username entries from the response
+                        if name in a:  # Check if a user with that name already exists
+                            self.status = 'A user with that username already exists. Please try again!'
+                            self.cool_down = 0
+                        else:  # If not
+                            # Insert the current user and commit the changes to the server
+                            sql = "INSERT INTO `users` (`Username`, `Password`) VALUES (%s, %s)"
+                            cursor.execute(sql, (name, pw))
+                            connection.commit()
+                            self.status = 'Operation executed successfully!'
+                            self.cool_down = 0
+                finally:  # Always close the connection
+                    connection.close()
+
             elif command == "login_asc":  # If the request asks the DB to check if user exists
                 if not SceneBase.logged_in[1]:  # and another user is not currently logged in from the session
                     try:
@@ -244,31 +250,15 @@ class AccountScene(SceneBase):
                                 self.status = 'Anti-spacecraft player signed in as [{0}]! Enjoy the game.'.format(name)
                                 SceneBase.credentials[1] = [name, pw]  # Update the state
                                 SceneBase.logged_in[1] = True
+                                self.cool_down = 0
                             else:
                                 self.status = 'Wrong credentials entered. Please check the input again.'
+                                self.cool_down = 0
                     finally:  # Always close the connection
                         connection.close()
                 else:
                     self.status = 'You are already signed in! Please log out first.'
-
-            elif command == "register_asc":  # If the DB has to register a new user
-                if not SceneBase.logged_in[1]:  # and one is not currently logged into another account
-                    try:
-                        with connection.cursor() as cursor:  # Create a cursor object
-                            sql = "SELECT `Username` FROM `users`"  # Define the statement in SQL
-                            cursor.execute(sql)  # Execute it
-                            a = [row[0] for row in cursor.fetchall()]  # Fetch all username entries from the response
-                            if name in a:  # Check if a user with that name already exists
-                                self.status = 'A user with that username already exists. Please try again!'
-                            else:  # If not
-                                # Insert the current user and commit the changes to the server
-                                sql = "INSERT INTO `users` (`Username`, `Password`) VALUES (%s, %s)"
-                                cursor.execute(sql, (name, pw))
-                                connection.commit()
-                                self.status = 'Operation executed successfully!'
-                    finally:  # Always close the connection
-                        connection.close()
-                else:
-                    self.status = 'You are already signed in! Please log out to proceed.'
+                    self.cool_down = 0
         except pymysql.err.OperationalError:
             self.status = 'The server is currently offline. Please try again later.'
+            self.cool_down = 0
