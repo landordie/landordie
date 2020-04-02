792a3484bbd20d73773208f0fafc43e8622318e7
denideni21 <denideni21@abv.bg>
2020-04-02 13:37:10 +0100

    s

--- classes/thruster.py
+++
@@ -1,9 +0,0 @@
-from .sprite_class import Sprite
-
-
-# TODO implement this if necessary
-class Thruster(Sprite):
-
-    def __init__(self):
-        super().__init__('frames/ogan.png')  # call Sprite initializer
-        self.activated_image = Sprite("frames/ogan2.png")
